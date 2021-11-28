eec7d71b8f5f01963eafeda83e0fa7e989e62d2d
umbat <farhanpratamax@gmail.com>
2021-11-28 10:27:40 +0700

    menambahkan adapter, menu, halaman help dan about. Ada error di DetailActivity.kt

--- app/src/main/java/com/example/itarsi_recyclerview/PenggunaAdapter.kt
+++
@@ -1,7 +0,0 @@
-package com.example.itarsi_recyclerview
-
-class PenggunaAdapter: RecyclerView.Adapter<PenggunaAdapter.PenggunaViewHolder>() {
-
-
-    inner class PenggunaViewHolder(view: View) :
-}
