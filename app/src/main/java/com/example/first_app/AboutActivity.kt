f70395955dc7e685fddbdb59c48b77215d6ecfce
umbat <farhanpratamax@gmail.com>
2022-01-02 12:51:39 +0700

    Mengupdate konten di AboutActivity.kt

--- app/src/main/java/com/example/first_app/AboutActivity.kt
+++ app/src/main/java/com/example/first_app/AboutActivity.kt
@@ -1,6 +1,9 @@
 package com.example.first_app
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -17,13 +20,25 @@ class AboutActivity : AppCompatActivity() {
         val aboutName = findViewById<TextView>(R.id.tv_about_nama)
         val aboutKet = findViewById<TextView>(R.id.tv_about_ket)
         val aboutDesc = findViewById<TextView>(R.id.tv_about_desc)
+        val aboutInsta = findViewById<Button>(R.id.instagram)
+        val aboutGithub = findViewById<Button>(R.id.github)
 
         aboutImages.load(R.drawable.my_photo) {
             crossfade(true)
             transformations(CircleCropTransformation())
         }
         aboutName.text = "Muhammad Farhan Pratama"
-        aboutKet.text = "Shinobi Konoha"
-        aboutDesc.text = "Lahir di Magelang dan hidup sebagai shinobi Konoha"
+        aboutKet.text = "Taruna Instrumentasi MKG"
+        aboutDesc.text = "Lahir pada 8 Juni 2000 di Magelang. Bercita-cita menjadi fullstack developer walaupun laptopnya medioker dan ilmunya masih sangat minim"
+        aboutInsta.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("https://www.instagram.com/farhannpratama/"))
+            startActivity(intent)
+        }
+        aboutGithub.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse("https://github.com/umbat"))
+            startActivity(intent)
+        }
     }
 }
